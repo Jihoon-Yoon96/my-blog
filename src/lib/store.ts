e1cfdd222b701b95382d6985e87df64da5ece649
Jihoon-Yoon96 <03470@naver.com>
2026-01-18 10:50:19 +0900

    검색 zustand

---
+++ src/lib/store.ts
@@ -0,0 +1,16 @@
+// src/lib/store.ts
+import { create } from 'zustand'
+
+interface SearchState {
+    isOpen: boolean
+    onOpen: () => void
+    onClose: () => void
+    toggle: () => void
+}
+
+export const useSearchStore = create<SearchState>((set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+    toggle: () => set((state) => ({ isOpen: !state.isOpen })),
+}))
